8e7cce23f4cfbc8278b6317c949c1b4928ec8a26
shubhig-474 <shubhigoyal474@gmail.com>
2019-07-06 14:43:51 +0530

    completed

---
+++ server/models/cities.js
@@ -0,0 +1,24 @@
+const db = require('../database');
+
+class cities {
+    static retrieveALL (callback){
+      db.query(`SELECT city_name from cities`,function(err,res){
+          if(err.error)
+            return callback(err);
+          callback(res);  
+      });
+    }
+    
+
+    static insert(city,callback){
+        db.query('INSERT INTO cities (city_name) VALUES ($1)', [city], (err, res) => {
+            if (err.error)
+              return callback(err);
+            callback(res); 
+
+        });
+
+    }
+}
+
+module.exports=cities;
---
+++ server/models/weather.js
@@ -0,0 +1,18 @@
+const request = require('request-promise');
+
+const API_KEY = 'efba7e0f14aa5ea433a9edb5f13430e6';
+class Weather {
+  static retrieveByCity (city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+      json: true
+    }).then((res) => {
+      callback(res);
+    }).catch((err) => {
+      console.log(err);
+      callback({ error: 'Could not reach OpenWeatherMap API.' });
+    });
+  }
+}
+
+module.exports = Weather;
